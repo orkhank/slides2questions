f93f8841a773be400dcf51a31a50e90638b4e735
orkhank <118054233+orkhank@users.noreply.github.com>
2024-05-02 22:18:30 +0300

    add code for models

---
+++ llm_utils.py
@@ -0,0 +1,69 @@
+from typing import Literal, Optional
+import transformers
+import torch
+import functools
+
+
+@functools.lru_cache(maxsize=1)
+def get_llama_pipeline() -> transformers.pipelines.TextGenerationPipeline:
+    model_id = "meta-llama/Meta-Llama-3-8B"
+    return transformers.pipeline(
+        "text-generation",
+        model=model_id,
+        model_kwargs={
+            "torch_dtype": torch.bfloat16,
+            "quantization_config": {
+                "load_in_4bit": True,
+                "bnb_4bit_compute_dtype": torch.bfloat16,
+            },
+        },
+        device_map="auto",
+    )
+
+
+@functools.lru_cache(maxsize=1)
+def get_gemma_pipeline() -> transformers.pipelines.TextGenerationPipeline:
+    model = "google/gemma-1.1-2b-it"
+    return transformers.pipeline(
+        "text-generation",
+        model=model,
+        model_kwargs={
+            "torch_dtype": torch.bfloat16,
+            "quantization_config": {
+                "load_in_4bit": True,
+                "bnb_4bit_compute_dtype": torch.bfloat16,
+            },
+        },
+        device_map="auto",
+    )
+
+
+def get_gemma_response(
+    input_text: str,
+    *,
+    max_new_tokens: int = 99999,
+    temperature: float = 0.7,
+    top_k: int = 50,
+    top_p: float = 0.95,
+    do_sample: bool = True,
+    streamer: Optional[Literal["TextStreamer"]] = None,
+    **kwargs,
+) -> str:
+    pipeline = get_gemma_pipeline()
+
+    messages = [
+        {"role": "user", "content": input_text},
+    ]
+    prompt = pipeline.tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    outputs = pipeline(
+        prompt,
+        max_new_tokens=max_new_tokens,
+        # do_sample=do_sample,
+        # temperature=temperature,
+        # top_k=top_k,
+        # top_p=top_p,
+        streamer=transformers.TextStreamer(pipeline.tokenizer) if streamer else None,
+    )
+    return outputs[0]["generated_text"][len(prompt) :]

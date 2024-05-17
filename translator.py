3218724d08c9ff321111c6ea8e6e480f50572890
orkhank <118054233+orkhank@users.noreply.github.com>
2024-05-17 11:28:27 +0300

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,76 @@
+import os
+import sys
+from typing import Optional
+import googletrans.models
+from load_dotenv import load_dotenv
+import deepl
+from abc import ABC, abstractmethod
+import googletrans  # type: ignore
+
+
+class Translator(ABC):
+    @abstractmethod
+    def translate_text(self, text: str, target_language: str) -> str:
+        pass
+
+
+class DeepL(Translator):
+    def __init__(self, auth_key: Optional[str] = None) -> None:
+        if auth_key is None:
+            load_dotenv()
+            self.auth_key = os.getenv("DEEPL_AUTH_KEY")
+            if self.auth_key is None:
+                raise ValueError("DeepL API key is not provided")
+        else:
+            self.auth_key = auth_key
+
+        self.translator = deepl.Translator(self.auth_key)
+
+    def translate_text(self, text, target_language):
+        return self.translator.translate_text(text, target_lang=target_language)
+
+    def get_source_languages(self):
+        return self.translator.get_source_languages()
+
+    def get_target_languages(self):
+        return self.translator.get_target_languages()
+
+    def get_usage(self):
+        return self.translator.get_usage()
+
+
+class Google(Translator):
+    def __init__(self):
+        self.translator = googletrans.Translator()
+
+    def translate_text(self, text: str, target_language: str = "en") -> str:
+        if target_language not in googletrans.LANGUAGES:
+            raise ValueError(f"Language {target_language} is not supported")
+        if len(text) == 0:
+            raise ValueError("Text is empty")
+        if not isinstance(text, str):
+            raise ValueError("Text is not a string")
+
+        translated_text = self.translator.translate(text, dest=target_language)
+
+        # sanity check
+        assert isinstance(translated_text, googletrans.models.Translated)
+
+        return translated_text.text
+
+    def get_supported_languages(self):
+        return googletrans.LANGUAGES
+
+    def detect_language(self, text: str) -> str:
+        return self.translator.detect(text).lang
+
+
+def main():
+    translator = DeepL()
+    print(translator.translate_text("Hello, how are you?", "DE"))
+    translator = Google()
+    print(translator.translate_text("Hello, how are you?", "de"))
+
+
+if __name__ == "__main__":
+    sys.exit(main())

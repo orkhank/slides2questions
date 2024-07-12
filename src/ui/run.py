fab22a4dece8be7d35d22feceea84107b3e7a3ea
orkhank <118054233+orkhank@users.noreply.github.com>
2024-07-12 11:11:13 +0300

    Remove UI code

--- src/ui/run.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from controllers.main_window_controller import MainPageController
-
-app = QApplication([])
-window = MainPageController()
-window.show()
-app.exec_()

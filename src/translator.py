ea6bef3da5e4e64ff78ea5a964537db5cf7a6454
orkhank <118054233+orkhank@users.noreply.github.com>
2024-07-12 11:13:27 +0300

    Remove `DeepL` translator

--- src/translator.py
+++ src/translator.py
@@ -2,8 +2,6 @@ import os
 import sys
 from typing import Optional
 import googletrans.models
-from load_dotenv import load_dotenv
-import deepl
 from abc import ABC, abstractmethod
 import googletrans  # type: ignore
 
@@ -14,53 +12,6 @@ class Translator(ABC):
         self, text: str, source_language: Optional[str], target_language: str
     ) -> str:
         pass
-
-
-class DeepL(Translator):
-    def __init__(self, auth_key: Optional[str] = None) -> None:
-        if auth_key is None:
-            load_dotenv()
-            self.auth_key = os.getenv("DEEPL_AUTH_KEY")
-            if self.auth_key is None:
-                raise ValueError("DeepL API key is not provided")
-        else:
-            self.auth_key = auth_key
-
-        self.translator = deepl.Translator(self.auth_key)
-
-    def translate_text(
-        self,
-        text: str,
-        source_language: Optional[str] = None,
-        target_language: str = "EN-US",
-    ) -> str:
-        if (
-            source_language is not None
-            and source_language.upper() not in self.get_source_languages()
-        ):
-            raise ValueError(f"Language {source_language} is not supported")
-        if len(text) == 0:
-            raise ValueError("Text is empty")
-        if not isinstance(text, str):
-            raise ValueError("Text is not a string")
-
-        translated_text = self.translator.translate_text(
-            text, source_lang=source_language, target_lang=target_language
-        )
-
-        # sanity check
-        assert isinstance(translated_text, deepl.TextResult)
-
-        return translated_text.text
-
-    def get_source_languages(self):
-        return {lang.code for lang in self.translator.get_source_languages()}
-
-    def get_target_languages(self):
-        return {lang.code for lang in self.translator.get_target_languages()}
-
-    def get_usage(self):
-        return self.translator.get_usage()
 
 
 class Google(Translator):
@@ -100,14 +51,3 @@ def get_language(text: str) -> str:
 
     # map code to full language name
     return googletrans.LANGUAGES[language]
-
-
-def main():
-    translator = DeepL()
-    print(translator.translate_text("Hello, how are you?", "DE"))
-    translator = Google()
-    print(translator.translate_text("Hello, how are you?", "de"))
-
-
-if __name__ == "__main__":
-    sys.exit(main())
